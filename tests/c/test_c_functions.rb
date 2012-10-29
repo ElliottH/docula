fe0e3f5e0335702598e02994b9753f522c052d02
Elliott Hillary <ejh67@cam.ac.uk>
2012-10-29 17:55:27 +0000

    Add rules for functions. Add tests for function parsing.

--- tests/c/test_c_functions.rb
+++ tests/c/test_c_functions.rb
@@ -11,7 +11,17 @@ class TestCFunctions < Test::Unit::TestCase
   end
 
   def test_declarations
+    assert_not_nil @parser.parse("int a();")
+    assert_not_nil @parser.parse("int a(int x);")
+    assert_not_nil @parser.parse("int b(int x, float y);")
 
+    assert_not_nil @parser.parse("inline union foo bar(int a, float b);")
+    assert_not_nil @parser.parse("inline struct foo* bar(int a, char* x);")
+
+    assert_nil @parser.parse("a();")
+    assert_nil @parser.parse("int a(")
+    assert_nil @parser.parse("int b);")
+    assert_nil @parser.parse("inline union bar();")
   end
 
   def test_definitions
