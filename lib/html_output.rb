c9b35b8832d11d1ce1d0d5a7d9020e98cd28015a
Elliott Hillary <ejh67@cam.ac.uk>
2013-03-06 14:49:24 +0000

    Output processing.

---
+++ lib/html_output.rb
@@ -0,0 +1,237 @@
+require 'lib/output'
+
+class HtmlOutput < Output
+  def initialize(db, options)
+    super
+
+    Dir.mkdir(options[:output]) unless File.directory? options[:output]
+
+    stylefile = File.join(options[:output], "style.css")
+    File.open(
+      stylefile, 'w'
+    ) { |f| f.write(@@stylesheet) } unless File.exist? stylefile
+  end
+
+  def output
+    files_html = header("Files") << "<body>\n"
+    files_html << "<table><thead><th>Files</th></thead><tbody>"
+
+    files.each do |row|
+      files_html << row(
+                      link(
+                        "#{File.basename(row['path'])}.html",
+                        File.basename(row['path'])
+                      ),
+                      row['docstring'].split("\n\n")[0]
+                    )
+      files_html << "</tbody></table></body></html>"
+      File.open(
+        File.join(@options[:output], "files.html"),
+        'w'
+      ) { |f| f.write(files_html) }
+
+      File.open(
+        File.join(@options[:output], "#{File.basename(row['path'])}.html"),
+        'w'
+      ) { |f| f.write(file_html(row)) }
+    end
+  end
+
+  def file_html(row)
+    file_html = header(File.basename(row['path'])) << "<body>\n"
+    file_html << "<div id=\"header\">\n"
+    file_html << "<h1>#{File.basename(row['path'])}</h1>\n"
+    file_html << "<ul id=\"links\">\n"
+    file_html << "<li><a href=\"#description\">description</a></li>\n"
+    file_html << "<li><a href=\"#defines\">#defines</a></li>\n"
+    file_html << "<li><a href=\"#typedefs\">typedefs</a></li>\n"
+    file_html << "<li><a href=\"#functions\">functions</a></li>\n"
+    file_html << "</ul></div><hr />"
+    file_html << "<div id=\"summary\">\n<h2>Summary</h2>\n"
+    file_html << "#{row['docstring'].split("\n")[0]}\n</div>\n<hr />"
+
+    includes_html(row)
+    file_html << defines_html(row['id'])
+    file_html << typedefs_html(row['id'])
+    file_html << functions_overview_html(row['id'])
+
+    file_html << "<hr />\n<h2 id=\"description\">Description</h2>\n"
+    file_html << "#{row['docstring']}"
+
+    file_html << variables_html(row['id'])
+    file_html << "<hr />\n<h2>Function Documentation</h2>\n"
+    file_html << functions_html(row['id'])
+  end
+
+  def includes_html(row)
+  end
+
+  def defines_html(row)
+    if defines(row).count > 0
+      html = "<table id=\"defines\" class=\"defs\">\n"
+      html << "<thead><th colspan=\"2\">#defines</th></thead>\n<tbody>\n"
+
+      defines(row).each do |define|
+        html << row_with_id("d#{define['id']}", define['name'], define['value'])
+        html << row("", define['docstring'])
+      end
+
+      html << "</tbody></table>\n"
+    else
+      ""
+    end
+  end
+
+  def typedefs_html(row)
+    if typedefs(row).count > 0
+      html = "<table id=\"typedefs\" class=\"defs\">\n"
+      html << "<thead><th colspan=\"2\">Typedefs</th></thead>\n<tbody>\n"
+
+      typedefs(row).each do |typedef|
+        html << row_with_id("t#{typedef['id']}", typedef['value'], typedef['name'])
+          html << row("", typedef['docstring'])
+      end
+
+      html << "</tbody></table>\n"
+    else
+      ""
+    end
+  end
+
+  def variables_html(row)
+    if variables(row).count > 0
+      ""
+    else
+      ""
+    end
+  end
+
+  def functions_overview_html(row)
+    if functions(row).count > 0
+      html = "<table id=\"functions\" class=\"defs\">\n"
+      html << "<thead><th colspan=\"2\">Functions</th></thead>\n<tbody>\n"
+
+      functions(row).each do |function|
+        html << "<tr><td>#{formatted_type(function['type'])}</td>"
+        html << "<td><a href=\"#f#{function['id']}\">#{function['name']}</a>( "
+
+        html << arguments(function['id']).map do |argument|
+          "#{argument['type']} #{argument['name']}"
+        end.join(", ")
+
+        html << " )</td></tr>"
+
+        if function['docstring']
+          html << row("", function['docstring'].split("\n\n")[0])
+        else
+          html << row()
+        end
+      end
+
+      html << "</tbody></table>\n"
+    else
+      ""
+    end
+  end
+
+  def functions_html(row)
+    functions(row).map do |function|
+      html = "<div id=\"f#{function['id']}\"><table><tbody><tr>"
+      html << "<td>#{function['type']}</td>"
+      html << "<td>#{function['name']}</td>"
+      html << "<td>(</td>"
+
+      html << arguments(function['id']).map do |argument|
+        "<td>#{argument['type']}</td><td>#{argument['name']}</td>"
+      end.join("</tr>\n<tr><td></td><td></td><td></td><td></td><td></td>")
+
+      html << "</tr>\n<tr><td></td><td></td><td>)</td></tr></tbody></table>"
+      html << "<span class=\"docstring\">#{function['docstring']}</span>"
+      html << "<span class=\"arguments\">Arguments:\n<dl>"
+
+      html << arguments(function['id']).map do |argument|
+        "<dt>#{argument['name']}</dt>"
+        "<dd><span class=\"flow\">#{argument['flow']}</span>"
+        "#{argument['docstring']}</dd>"
+      end.join("\n")
+
+      html << "</dl></span>"
+      html << "<span class=\"returns\">Returns:\n#{function['return']}</span>"
+      html << "</div>"
+    end.join("\n")
+  end
+
+  def row(*entries)
+    row_with_id("", entries)
+  end
+
+  def formatted_type(type)
+    info = type(type)
+    if info
+      href = "#{File.basename(info['path'])}.html#"
+      href << (info['define'] ? "d" : "t")
+      href << info['id'].to_s
+      link(href, type)
+    else
+      type
+    end
+  end
+
+  def row_with_id(id, *entries)
+    if entries == []
+      "<tr id=\"#{id}\"></tr>"
+    else
+      "<tr id=\"#{id}\"><td>#{entries.join("</td><td>")}</td></tr>\n"
+    end
+  end
+
+  def link(link, contents)
+    "<a href=\"#{link}\">#{contents}</a>"
+  end
+
+  def header(title)
+    <<-eos.unindent
+      <html>
+        <head>
+          <link rel=\"stylesheet\" href=\"style.css\" type=\"text/css\" />
+          <title>#{title}</title>
+        </head>
+    eos
+  end
+
+  @@stylesheet = <<-eos.unindent
+    div#header {
+      width: 100%;
+      text-align: center;
+    }
+    div#header ul {
+      list-style-type: none;
+      padding: 0;
+    }
+    div#header li {
+      display: inline;
+      margin: 0 0.5em;
+    }
+    table {
+
+    }
+    table th {
+      width: 100%;
+      border-bottom: 1px solid black;
+    }
+    table.defs {
+      width: 100%;
+      border: 1px solid black;
+      margin-bottom: 3em;
+    }
+    table.defs tbody tr td:first-child {
+      text-align: right;
+    }
+    table.defs tbody tr:nth-child(even) {
+      color: #555;
+    }
+    span.docstring, span.arguments, span.returns {
+      display: block;
+    }
+  eos
+end
---
+++ lib/output.rb
@@ -0,0 +1,65 @@
+class Output
+  def initialize(db, options)
+    @options = options
+
+    @files_stmt = db.prepare(
+      "SELECT id, path, includes, docstring FROM files"
+    )
+    @funcs_stmt = db.prepare(
+      "SELECT id, name, type, return, docstring FROM functions
+       WHERE file_id = ?"
+    )
+    @args_stmt = db.prepare(
+      "SELECT id, name, type, flow, docstring FROM arguments
+       WHERE func_id = ?"
+    )
+    @vars_stmt = db.prepare(
+      "SELECT id, name, type, docstring FROM variables
+       WHERE file_id = ?"
+    )
+    @types_stmt = db.prepare(
+      "SELECT id, name, value, docstring FROM typedefs
+       WHERE file_id = ?"
+    )
+    @defs_stmt = db.prepare(
+      "SELECT id, name, value, docstring FROM defines
+       WHERE file_id = ?"
+    )
+
+    @type_stmt = db.prepare(
+      "SELECT types.*, files.path FROM
+       (SELECT id, file_id, name, 'typedef' FROM typedefs WHERE typedefs.name = ?
+        UNION ALL
+        SELECT id, file_id, name, 'define' FROM defines WHERE defines.name = ?
+       ) AS 'types' INNER JOIN files ON types.file_id = files.id"
+    )
+  end
+
+  def files
+    @files_stmt.execute
+  end
+
+  def functions(file_id)
+    @funcs_stmt.execute(file_id)
+  end
+
+  def arguments(func_id)
+    @args_stmt.execute(func_id)
+  end
+
+  def variables(file_id)
+    @vars_stmt.execute(file_id)
+  end
+
+  def typedefs(file_id)
+    @types_stmt.execute(file_id)
+  end
+
+  def defines(file_id)
+    @defs_stmt.execute(file_id)
+  end
+
+  def type(name)
+    @type_stmt.execute(name, name).first
+  end
+end
